e7af369f116f3f389816109f553abb05c23352db
LapaJr31 <jarlat04@lapuas.xyz>
2023-11-04 12:22:39 +0100

    Created basic template

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type webhookReqBody struct {
+	Message struct {
+		Text string `json:"text"`
+		Chat struct {
+			ID int64 `json:"id"`
+		} `json:"chat"`
+	} `json:"message"`
+}
+
+type sendMsgReqBody struct {
+	ChatID int64  `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+func handler(res http.ResponseWriter, req *http.Request) {
+	body := &webhookReqBody{}
+	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
+		fmt.Println("could not decode request body", err)
+		return
+	}
+
+	if !strings.Contains(strings.ToLower(body.Message.Text), "marco") {
+		return
+	}
+
+	if err := sayPolo(body.Message.Chat.ID); err != nil {
+		fmt.Println("error in sending reply:", err)
+		return
+	}
+
+	fmt.Println("reply sent")
+}
+
+func sayPolo(chatID int64) error {
+	reqBody := &sendMsgReqBody{
+		ChatID: chatID,
+		Text:   "Polo!",
+	}
+	
+	reqBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post("https://api.telegram.org/bot6794417134:AAGQJvp8ciICkUsOlwR2CEusDdledEB3gkA/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return errors.New("unexpected status" + res.Status)
+	}
+
+	return nil
+}
+
+func main () {
+	http.ListenAndServe(":3000", http.HandlerFunc(handler))
+}
+
